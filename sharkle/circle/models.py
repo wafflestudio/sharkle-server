03380fb52a5a24a8aafbbf34642a426ef5373602
yabsed <yabsed@snu.ac.kr>
2022-02-24 20:03:31 +0900

    homepage model

--- sharkle/circle/models.py
+++ sharkle/circle/models.py
@@ -4,13 +4,23 @@ from user.models import CustomUser as User
 # Create your models here.
 from common.models import BaseModel
 
+class Hompage(BaseModel):
+    homepage1 = models.CharField(max_length=500, null=True, blank=False)
+    homepage2 = models.CharField(max_length=500, null=True, blank=False)
+    facebook = models.CharField(max_length=500, null=True, blank=False)
+    instagram = models.CharField(max_length=500, null=True, blank=False)
+    twitter = models.CharField(max_length=500, null=True, blank=False)
+    youtube = models.CharField(max_length=500, null=True, blank=False)
+    tiktok = models.CharField(max_length=500, null=True, blank=False)
+    band = models.CharField(max_length=500, null=True, blank=False)
 
 class Circle(BaseModel):
     type = models.IntegerField()
     name = models.CharField(max_length=100, null=False, blank=False)
     bio = models.CharField(max_length=300, null=False, blank=True)
-    homepage = models.CharField(max_length=300, null=True, blank=False)
+    homepage = models.ForeignKey(Hompage, on_delete=models.SET_NULL, null=True)
     introduction = models.CharField(max_length=5000, null=True, blank=True)
+    tag = models.CharField(max_length=500, null=False, blank=True)
 
     def __str__(self):
         return self.name

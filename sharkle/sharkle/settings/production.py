e4371dda7090f67685bbd0e28594a8579e1b906d
ding-young <lsyhime@snu.ac.kr>
2022-03-06 20:49:18 +0900

    Feat: logging

--- sharkle/sharkle/settings/production.py
+++ sharkle/sharkle/settings/production.py
@@ -18,3 +18,65 @@ DATABASES = {
         },
     }
 }
+
+# CUSTOM LOGGING
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{server_time}] {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        # added for saving log files
+        "file": {
+            "level": "DEBUG",
+            "filters": [
+                "require_debug_false"
+            ],  # later change to debug_false (only for server)
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "django.server",
+            "encoding": "UTF-8",
+            "filename": os.path.join(BASE_DIR, "logging/sharkle-server.log"),
+            "maxBytes": 1024 * 1024 * 5,  # TODO 5 MB
+            "backupCount": 5,
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins", "file"],
+            # 'level': 'INFO',
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}

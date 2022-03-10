299dbb67013493f29b89db46775217bd5f82f544
ding-young <lsyhime@snu.ac.kr>
2022-03-10 22:22:45 +0900

    Fix: CI mysql socket error

--- sharkle/sharkle/settings/development.py
+++ sharkle/sharkle/settings/development.py
@@ -4,23 +4,23 @@ from sharkle.settings.base import *
 DEBUG = True
 
 if DEBUG:
-    INSTALLED_APPS.append('debug_toolbar')
-    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-    INTERNAL_IPS = ['127.0.0.1']
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = ["127.0.0.1"]
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'NAME': 'sharkle',
-        'USER': os.environ.get(('DB_USER')),
-        'PASSWORD': os.environ.get(('DB_PASSWORD')),
-        'OPTIONS': { # for emoji
-            'charset': 'utf8mb4',
-            'use_unicode': True,
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "127.0.0.1",
+        "PORT": 3306,
+        "NAME": "sharkle",
+        "USER": os.environ.get(("DB_USER")),
+        "PASSWORD": os.environ.get(("DB_PASSWORD")),
+        "OPTIONS": {  # for emoji
+            "charset": "utf8mb4",
+            "use_unicode": True,
         },
     }
 }

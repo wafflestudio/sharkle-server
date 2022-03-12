b4ec7a6cdbbfd8d283ba02cb79842afd31c8b752
ding-young <lsyhime@snu.ac.kr>
2022-03-12 23:32:03 +0900

    token refresh, verify view

--- sharkle/user/urls.py
+++ sharkle/user/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
-from user.views import PingPongView, SignUpView, TokenObtainPairView
+from user.views import PingPongView, SignUpView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("ping/", PingPongView.as_view(), name="ping"),  # /api/v1/ping/
@@ -8,4 +13,10 @@ urlpatterns = [
     path(
         "auth/login/", TokenObtainPairView.as_view(), name="login"
     ),  # /api/v1/auth/login/
+    path(
+        "auth/token/refresh/", TokenRefreshView.as_view(), name="refresh"
+    ),  # /api/v1/auth/token/refresh/
+    path(
+        "auth/token/verify/", TokenVerifyView.as_view(), name="verify"
+    ),  # /api/v1/auth/token/verify/
 ]

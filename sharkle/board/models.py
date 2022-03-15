51e62cee62badf320d811efe55441111b3265b34
nemga123 <hspark0419@naver.com>
2022-03-15 21:40:44 +0900

    33/Implement viewset

--- sharkle/board/models.py
+++ sharkle/board/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+from circle.models import Circle
+
+
 class Board(models.Model):
-    circle = models.SmallIntegerField(null=False)  # models.ForeignKey(Circle)
+    circle_id = models.ForeignKey(
+        Circle, on_delete=models.CASCADE, related_name="boards"
+    )  # models.ForeignKey(Circle)
     name = models.CharField(max_length=20, blank=False)
     is_private = models.BooleanField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
--- sharkle/board/views.py
+++ sharkle/board/views.py
@@ -4,8 +4,7 @@ from rest_framework.response import Response
 from board.models import Board
 from board.serializers import BoardSerializer
 
-
-# Create your views here.
+from circle.models import Circle, UserCircle_Member
 
 
 class BoardViewSet(viewsets.GenericViewSet):
@@ -14,14 +13,10 @@ class BoardViewSet(viewsets.GenericViewSet):
 
     def create(self, request, **kwargs):
         data = request.data
-        user = request.user
-        # 유저 권한 인증
-        circle_id = self.kwargs["circle_id"]
-        if circle_id != request.data["circle"]:
-            return Response(
-                "주소의 동아리 id와 데이터의 id가 다릅니다.", status=status.HTTP_400_BAD_REQUEST
-            )
-        # 동아리 존재 여부 검증
+
+        if (response := self.circle_validate(request, kwargs)) is not None:
+            return response
+
         serializer = BoardSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         board = serializer.save()
@@ -62,6 +57,8 @@ class BoardViewSet(viewsets.GenericViewSet):
             return Response("해당하는 게시판을 찾을 수 없습니다.", status=status.HTTP_404_NOT_FOUND)
 
     def destroy(self, request, pk=None, **kwargs):
+        if (response := self.circle_validate(request, kwargs)) is not None:
+            return response
         if self.get_queryset().filter(id=pk).exists():
             circle_id = self.kwargs["circle_id"]
             board = self.get_queryset().filter(id=pk).first()
@@ -75,3 +72,34 @@ class BoardViewSet(viewsets.GenericViewSet):
             )
         else:
             return Response("해당하는 게시판을 찾을 수 없습니다.", status=status.HTTP_404_NOT_FOUND)
+
+    def circle_validate(self, request, **kwargs):
+        # 로그인 여부 검증
+        user = request.user
+        if user is None:
+            return Response("로그인이 필요한 서비스입니다.", status=status.HTTP_401_UNAUTHORIZED)
+
+        # url상의 circle id와 데이터 상의 circle id가 같은지 검증
+        circle_id = self.kwargs["circle_id"]
+        if circle_id != request.data["circle"]:
+            return Response(
+                "주소의 동아리 id와 데이터의 id가 다릅니다.", status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 동아리 존재 여부 검증
+        if not Circle.objects.filter(id=circle_id).exists():
+            return Response(
+                "id: " + str(circle_id) + "에 해당하는 동아리가 존재하지 않습니다.",
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        member = UserCircle_Member.objects.filter(user_id=user.id, circle_id=circle_id)
+        # 유저가 동아리 구성원인지 검증
+        if not member.exists():
+            return Response("해당 동아리에 가입되지 않았습니다.", status=status.HTTP_401_UNAUTHORIZED)
+        # 유저가 동아리 관리자인지 검증
+        if not member.first().is_manager:
+            return Response(
+                "동아리 게시판은 관리자만 생성할 수 있습니다.", status=status.HTTP_401_UNAUTHORIZED
+            )
+        return None

d7e7ab4001c63600e7f0370e865d5ca2f0775f5d
nemga123 <hspark0419@naver.com>
2022-03-25 04:17:06 +0900

    54/BoardModelTest-creataion test

--- sharkle/board/tests.py
+++ sharkle/board/tests.py
@@ -1,66 +1,78 @@
+import factory
 from django.test import TestCase
 
 # Create your tests here.
 from django.test import TestCase
+from factory.django import DjangoModelFactory
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from circle.models import Circle, UserCircle_Member
 from circle.serializers import CircleSerializer
 from rest_framework import status
 
+from user.models import User
 from user.serializers import UserSignUpSerializer
 
 
-class TestUser:
-    not_member_user = {
-        "email": "notMemeber@snu.ac.kr",
-        "password": "password",
-        "username": "user1",
-        "user_id": "user1",
-    }
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
 
-    not_manager_user = {
-        "email": "notManager@snu.ac.kr",
-        "password": "password",
-        "username": "user2",
-        "user_id": "user2",
-    }
-
-    manager_user = {
-        "email": "Manager@snu.ac.kr",
-        "password": "password",
-        "username": "user3",
-        "user_id": "user3",
-    }
+    @classmethod
+    def create(cls, **kwargs):
+        user = User.objects.create_user(**kwargs)
+        return user
 
 
-class TestCircle:
-    circle = {
-        "name": "waffle",
-        "bio": "waffle_circle",
-        "tag": "프로그래밍",
-        "type0": 0,
-        "type1": 0,
-    }
+class NotMemberFactory:
+    def __init__(self):
+        self.user = UserFactory(
+            email="notMemeber@snu.ac.kr",
+            password="password",
+            username="user1",
+            user_id="user1",
+        )
+
+
+class NotManagerFactory:
+    def __init__(self):
+        self.user = UserFactory(
+            email="notmanager@snu.ac.kr",
+            password="password",
+            username="user2",
+            user_id="user2",
+        )
+
+
+class ManagerFactory:
+    def __init__(self):
+        self.user = UserFactory(
+            email="Manager@snu.ac.kr",
+            password="password",
+            username="user3",
+            user_id="user3",
+        )
+
+
+class CircleFactory(DjangoModelFactory):
+    class Meta:
+        model = Circle
+
+    name = "waffle"
+    bio = "wafflr_circle"
+    tag = "프로그래밍"
+    type0 = 0
+    type1 = 0
 
 
 class BoardCreateTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user_serializer1 = UserSignUpSerializer(data=TestUser.not_member_user)
-        user_serializer1.is_valid()
-        cls.user1 = user_serializer1.save()  # User1 is manager of circle
-        user_serializer1.is_valid()
-        user_serializer2 = UserSignUpSerializer(data=TestUser.not_manager_user)
-        user_serializer2.is_valid()
-        cls.user2 = user_serializer2.save()  # User2 is only member of circle
-        user_serializer3 = UserSignUpSerializer(data=TestUser.manager_user)
-        user_serializer3.is_valid()
-        cls.user3 = user_serializer3.save()  # User3 is not member of circle
+        cls.user1 = NotMemberFactory().user  # User1 is manager of circle
+        cls.user2 = NotManagerFactory().user  # User2 is only member of circle
+        cls.user3 = ManagerFactory().user  # User3 is not member of circle
 
-        circle_serializer = CircleSerializer(data=TestCircle.circle)
-        circle_serializer.is_valid()
-        cls.circle = circle_serializer.save()
+        cls.circle = CircleFactory()
 
         UserCircle_Member.objects.create(
             circle_id=cls.circle.id, user_id=cls.user1.id, is_manager=True
@@ -77,6 +89,7 @@ class BoardCreateTestCase(TestCase):
 
     def test_create_board_success(self):
         data = self.board_data.copy()
+        print(Circle.objects.all())
         token = self.user1_token
         response = self.client.post(
             f"/api/v1/circle/{self.circle.id}/board/",
@@ -84,7 +97,6 @@ class BoardCreateTestCase(TestCase):
             content_type="application/json",
             HTTP_AUTHORIZATION=token,
         )
-
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_data = response.json()
         self.assertIn("id", response_data)
@@ -99,7 +111,6 @@ class BoardCreateTestCase(TestCase):
             content_type="application/json",
             HTTP_AUTHORIZATION=token,
         )
-
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_board_with_not_member_user(self):
@@ -111,5 +122,4 @@ class BoardCreateTestCase(TestCase):
             content_type="application/json",
             HTTP_AUTHORIZATION=token,
         )
-
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)

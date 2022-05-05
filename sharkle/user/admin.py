48e267e2694661061ae6db3585044ab897dff963
ding-young <lsyhime@snu.ac.kr>
2022-05-05 10:44:05 +0900

    base admin (list)

--- sharkle/user/admin.py
+++ sharkle/user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from user.models import User
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "user_id"]
+    list_display_links = ["id", "username", "email", "user_id"]
+    search_fields = ["id", "username", "email", "user_id"]
+
+
+admin.site.register(User, UserAdmin)

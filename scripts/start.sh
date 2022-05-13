a15507280cb4a59f1f69a932beda5767430a3a92
nemga123 <66145493+nemga123@users.noreply.github.com>
2022-05-14 01:52:08 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,8 @@ WORKING_DIR=/home/ec2-user/build/sharkle
 export PATH=/home/ec2-user/.pyenv/bin:/home/ec2-user/.pyenv/plugins/pyenv-virtualenv/shims:/home/ec2-user/.pyenv/shims:/home/ec2-user/.pyenv/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 
 source /home/ec2-user/.bashrc
+echo "[Deploy] : Kill old uwsgi process"
+sudo pkill -f uwsgi -9
 
 echo "[Deploy] : Activate virtual env"
 pyenv activate $VIRTUAL_ENV
